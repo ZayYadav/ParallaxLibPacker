61cdf9b6675dfa492347d6c0fe351d3725914161
ZayYadav <jyadavyadav989878@gmail.com>
2026-09-02 14:41:59 +0530

    Upload complete REVERSEeNGINE project

---
+++ upx-devel/src/stub/src/MAX_ELF_HDR.c
@@ -0,0 +1,48 @@
+/* MAX_ELF_HDR.c
+   created from MAX_ELF_HDR.h, 215 (0xd7) bytes
+
+   This file is part of the UPX executable compressor.
+
+   Copyright (C) Markus Franz Xaver Johannes Oberhumer
+   Copyright (C) Laszlo Molnar
+   Copyright (C) John F. Reiser
+   All Rights Reserved.
+
+   UPX and the UCL library are free software; you can redistribute them
+   and/or modify them under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.
+   If not, write to the Free Software Foundation, Inc.,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+   Markus F.X.J. Oberhumer              Laszlo Molnar
+   <markus@oberhumer.com>               <ezerotven+github@gmail.com>
+
+   John F. Reiser
+   <jreiser@users.sourceforge.net>
+ */
+
+/* clang-format off */
+
+/* 0x0000 */  35,112,114, 97,103,109, 97, 32,111,110, 99,101, 10, 47, 47, 32,
+/* 0x0010 */  66,111,117,110,100, 32,111,110, 32, 98,121,116,101, 32,108,101,
+/* 0x0020 */ 110,103,116,104, 32,111,102, 32, 69, 76, 70, 32,104,101, 97,100,
+/* 0x0030 */ 101,114,115, 32, 40, 69,104,100,114, 32, 43, 32, 80,104,100,114,
+/* 0x0040 */  91, 93, 41, 46, 10, 47, 47, 32, 80,111,115,115,105, 98,108,101,
+/* 0x0050 */  32, 51, 45, 98,121,116,101, 32,111,118,101,114,114,117,110, 32,
+/* 0x0060 */ 100,117,114,105,110,103, 32,101,120,112, 97,110,115,105,111,110,
+/* 0x0070 */  32,111,102, 32, 78, 82, 86, 50, 66, 10, 47, 47, 32,109,117,115,
+/* 0x0080 */ 116, 32, 98,101, 32,104, 97,110,100,108,101,100, 32,105,110, 32,
+/* 0x0090 */  58, 58, 99, 97,110, 80, 97, 99,107, 32,101,116, 99, 46, 10, 35,
+/* 0x00a0 */ 100,101,102,105,110,101, 32, 77, 65, 88, 95, 69, 76, 70, 95, 72,
+/* 0x00b0 */  68, 82, 95, 51, 50, 32, 49, 48, 50, 52, 10, 35,100,101,102,105,
+/* 0x00c0 */ 110,101, 32, 77, 65, 88, 95, 69, 76, 70, 95, 72, 68, 82, 95, 54,
+/* 0x00d0 */  52, 32, 52, 48, 57, 54, 10
